957790ba4dc2f758627f327afd5204ebc532e857
alex <ferry.ale@gmail.com>
2016-05-03 19:51:37 +0200

    Added security and login/logout options.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 from .models import Post
 from .forms import PostForm
 
@@ -9,33 +10,63 @@ def post_list(request):
 	posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts': posts})
 	
+def post_draft_list(request):
+	posts = Post.objects.filter(published_date__isnull = True).order_by('created_date')
+	return render(request, 'blog/post_draft_list.html', {'posts': posts})
+	
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk = pk)
 	return render(request, 'blog/post_detail.html',  {'post': post})
+
+def authorization_error(request):
+	return render(request, 'blog/authorization_error.html')
 	
+@login_required	
 def post_new(request):
 	if request.method == "POST":
 		form = PostForm(request.POST)
 		if form.is_valid():
 			post = form.save(commit = False)
 			post.author = request.user
-			post.published_date = timezone.now()
 			post.save()
 			return redirect('post_detail', pk = post.pk)
 	else:
 		form = PostForm()
 	return render(request, 'blog/post_edit.html', {'form': form})
-	
+
+@login_required		
 def post_edit(request, pk):
 	post = get_object_or_404(Post, pk = pk)
-	if request.method == "POST":
-		form = PostForm(request.POST, instance = post)
-		if form.is_valid():
-			post = form.save(commit = False)
-			post.author = request.user
-			post.published_date = timezone.now()
-			post.save()
-			return redirect('post_detail', pk = post.pk)
+	if(post.author == request.user):
+		if request.method == "POST":
+			form = PostForm(request.POST, instance = post)
+			if form.is_valid():
+				post = form.save(commit = False)
+				post.author = request.user
+				post.published_date = timezone.now()
+				post.save()
+				return redirect('post_detail', pk = post.pk)		
+		else:
+			form = PostForm(instance = post)
 	else:
-		form = PostForm(instance = post)
+		return redirect('authorization_error')	
 	return render(request, 'blog/post_edit.html', {'form': form})
+
+@login_required	
+def post_publish(request, pk):
+	post = get_object_or_404(Post, pk = pk)
+	if(post.author == request.user):
+		post.publish()
+	else:
+		return redirect('authorization_error')
+	return redirect('blog.views.post_list')
+	
+@login_required	
+def post_remove(request, pk):
+	post = get_object_or_404(Post, pk = pk)
+	if(post.author == request.user):
+		post.delete()
+	else:
+		return redirect('authorization_error')
+	return redirect('blog.views.post_list')
+	
