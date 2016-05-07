8cd905be2d69fffa02ff84de06279e2f40c0b61e
alex <ferry.ale@gmail.com>
2016-05-07 14:35:48 +0200

    Minor template and setting changes.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!(te1*x0q97t&=kx17hvw0o5u=4omwjx1#66rcu+uqtr(_q7fj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['wannashape.pythonanywhere.com']
 
 
 # Application definition
